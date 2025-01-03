c510db65a4e83199f570025afe74eb277f3942d0
유일한 <yih11911@tukorea.ac.kr>
2025-01-03 20:21:43 +0900

    remove .git file

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    // API 요청
+    fetch('/api')
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+  }, []); // 빈 배열은 한 번만 실행되도록 함
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
